83fe519e7f7ab8f5790f23203c7f18fa79343b01
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-01 16:40:58 -0700

    update

---
+++ Grokking/8_DepthFirstSearch/0_binaryTreePathSum.js
@@ -0,0 +1,5 @@
+// Prompt: Given a binary tree and a number 'S', find if the tree has a path from root-to-leaf so the sum of all node values of that path equals 'S'.
+// ! Question number 89
+
+
+// Solutiont: Use DFS. Start from the root and make recursive calls one left and one right.
