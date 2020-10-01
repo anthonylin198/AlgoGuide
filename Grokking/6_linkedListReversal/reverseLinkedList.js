deb8a9c5688953ae51053867652ead52ab8a283f
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-01 14:44:31 -0700

    reverseLinkedList

---
+++ Grokking/6_linkedListReversal/reverseLinkedList.js
@@ -0,0 +1,42 @@
+// given the head of a singly linkedlist, reverse the linkedlist. write a function to return then ew head of the reversed linkedlist ( 69 )
+
+
+
+// Solution: Reverse one node at a time. Start with the variable current, which will initially point to the head and a variable previous. Previous will point to null
+// We will reverse the current node by pointing it to the previous before moving on to the next node. 
+// Time complexity is O(n) where 'N' is the total number of nodes in the linkedList
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+  print_list() {
+    let temp = this;
+    while (temp !== null) {
+      process.stdout.write(`${temp.value} `);
+      temp = temp.next;
+    }
+    console.log();
+  }
+}
+function reverse(head) {
+  let current = head,
+    previous = null;
+  while (current !== null) {
+    next = current.next; // temporarily store the next node
+    current.next = previous; // reverse the current node
+    previous = current; // before we move to the next node, point previous to the current 
+    current = next; // move on the next node
+  }
+  return previous;
+}
+
+const head = new Node(2);
+head.next = new Node(4);
+head.next.next = new Node(6);
+head.next.next.next = new Node(8);
+head.next.next.next.next = new Node(10);
+
+process.stdout.write('Nodes of original LinkedList are: '); head.print_list();
+result = reverse(head);
+process.stdout.write('Nodes of reversed LinkedList are: '); result.print_list();
