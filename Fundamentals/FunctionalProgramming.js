ddc7e0b712e61c5a951d1290478ce400ea7c97b2
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-07 08:34:40 -0700

    update

---
+++ Fundamentals/FunctionalProgramming.js
@@ -0,0 +1,56 @@
+// ! Keep functions pure without sideeffects, and look into filter vs map vs reduce vs foreach
+
+
+// todo: forEach - for every element on the array, we are calling a callback function which gets element and its index. Works as a traditional for loop looping over the array and proving array elements to do operatons on them
+const sample = [1, 2, 3];
+
+// es5
+// sample.forEach(function (elem, index) {
+//   console.log(elem + ' comes at ' + index);
+// })
+
+// es6
+sample.forEach((elem, index) => `${elem} comes at ${index}`);
+
+// console.log(sample);
+
+
+
+
+// todo: Filter - whenever nede to filter an array - provide a callback for every slement and return a filter array - returns a filtered array
+// es5
+// const result = sample.filter(function (elem) {
+//   return elem !== 2;
+// })
+
+
+// es6
+const result = sample.filter(elem => elem !== 2)
+
+console.log(result)
+
+
+// todo: Map - takes a callback and runs on every element in an array, but GENERATES NEW ARRAY BASED ON EXISTING ARRAY
+const sample2 = [1, 2, 3]
+
+// es5
+// const mapped = sample.map(function (elem) {
+//   return elem * 10;
+// })
+
+// es6 
+const mapped = sample2.map(elem => elem * 10)
+// console.log(mapped)
+
+
+// todo: Reduce - Reduces the array to a single value. For example, to add all elements of an array
+const sample3 = [1, 2, 3]
+
+// es5
+// const sum = sample.reduce(function(sum, elem) {
+//   return sum + elem
+// })
+
+// es6
+const sum = sample3.reduce((sum, elem) => sum + elem)
+// console.log(sum)
--- scratchpad.js
+++ scratchpad.js
@@ -1,24 +1,106 @@
-
-
-// Get O(nlogn) timee and O(1) space
-function twoSum(arr, targetSum) {
-  // sort the arr in ascending
-  arr.sort((a, b) => a - b)
-  // implement the two pointer approach to find target sum 
-  let start = 0;
-  let end = arr.length - 1;
-  while (start < end) {
-    const currentSum = arr[start] + arr[end];
-    if (currentSum === targetSum) {
-      return [arr[start], arr[end]];
-    } else if (currentSum < targetSum) {
-      start++;
-    } else if (currentSum > targetSum) {
-      end--;
+const getRefillContainers = (boxData) => {
+  // declare array to store all boxes
+  const boxes = [];
+  // declare object to store the remaining brushes
+  const brushes = {};
+  // Use forEach to iterate through boxData, add in every group - potentially filter every above 2
+  boxData.forEach((elem) => {
+    // if it does not exist, declare key value pair
+    if (!(elem.brush_color in brushes)) {
+      brushes[elem.brush_color] = 1;
+    } else {
+      brushes[elem.brush_color]++;
+      if (brushes[elem.brush_color] === 4) {
+        const box = {};
+        box[elem.brush_color] = 4;
+        boxes.push(box);
+        delete brushes[elem.brush_color];
+      }
     }
+  });
+
+  // sort the array
+  const sortable = [];
+  for (let key in brushes) {
+    sortable.push([key, brushes[key]]);
   }
+  sortable.sort(function (a, b) {
+    return b[1] - a[1];
+  });
 
-  return [];
-}
+  // iterate through the remaining singles
+  const remainingBrushes = Object.keys(brushes);
+  let obj = {};
+  let counter = 0;
+  sortable.forEach((key) => {
+    for (let i = 0; i < brushes[key[0]]; i++) {
+      if (!(key[0] in obj)) {
+        // check if it doesn't exist
+        obj[key[0]] = 1;
+        counter++;
+        if (counter === 4) {
+          boxes.push(obj);
+          obj = {};
+          counter = 0;
+        }
+      } else {
+        obj[key[0]]++;
+        counter++;
+        // see if the box has 4 elements
+        if (counter === 4) {
+          boxes.push(obj);
+          obj = {};
+          counter = 0;
+        }
+      }
+    }
+  });
 
-console.log(twoSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
+  if (Object.keys(obj).length) boxes.push(obj);
+  return boxes;
+};
+
+
+const arr = [
+  {
+    brush_color: "blue",
+  },
+  {
+    brush_color: "blue",
+  },
+  {
+    brush_color: "blue",
+  },
+  {
+    brush_color: "blue",
+  },
+  {
+    brush_color: "blue",
+  },
+  {
+    brush_color: "blue",
+  },
+  {
+    brush_color: "blue",
+  },
+  {
+    brush_color: "blue",
+  },
+  {
+    brush_color: "green",
+  },
+  {
+    brush_color: "green",
+  },
+  {
+    brush_color: "green",
+  },
+  {
+    brush_color: "pink",
+  },
+  {
+    brush_color: "pink",
+  },
+];
+
+console.log(getRefillContainers(arr))
