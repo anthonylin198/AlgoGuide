2f8edc1bb213f72ba712e8d18fca67d401f01086
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-07 09:13:22 -0700

    sum update

--- Data Structures/1. Arrays/Medium/ThreeNumberSum.js
+++ Data Structures/1. Arrays/Medium/ThreeNumberSum.js
@@ -3,6 +3,7 @@
 // ! brute force solution
 function threeNumberSum(array, targetSum) {
   // create the sumArray that will start of empty, will contain all triplets
+  array.sort((a, b) => a - b)
   const sumArray = [];
   // outter for loop will start from i = 0
   for (let i = 0; i < array.length - 2; i++) {
@@ -17,4 +18,31 @@ function threeNumberSum(array, targetSum) {
   return sumArray;
 }
 
-console.log(threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0));
+// console.log(threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0));
+
+
+// Two pointer approach O(n^2) time and O(1) space
+function threeNumberSum2(array, targetSum) {
+  // sort the array in ascending order
+  array.sort((a, b) => a - b);
+  const sumArray = [];
+  for (let i = 0; i < array.length - 2; i++) {
+    // implement 2 pointer approach
+    let left = i + 1;
+    let right = array.length - 1;
+    while (left < right) {
+      const currentSum = array[i] + array[left] + array[right];
+      if (currentSum === targetSum) {
+        sumArray.push([array[i], array[left], array[right]])
+        right-- // we know it is less
+      } else if (currentSum < targetSum) {
+        left++
+      } else if (currentSum > targetSum) {
+        right--
+      }
+    }
+  }
+  return sumArray
+}
+
+console.log(threeNumberSum2([12, 3, 1, 2, -6, 5, -8, 6], 0));
