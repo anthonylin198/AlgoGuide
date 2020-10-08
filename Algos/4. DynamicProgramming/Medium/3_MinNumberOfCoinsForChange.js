6ec1476959fe7aaf6867f03071a702e16b0cc5c5
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-08 16:14:01 -0700

    minNumber update

--- Algos/4. DynamicProgramming/Medium/3_MinNumberOfCoinsForChange.js
+++ Algos/4. DynamicProgramming/Medium/3_MinNumberOfCoinsForChange.js
@@ -22,3 +22,44 @@ function minNumberOfCoinsForChange(n, denoms) {
   }
   return numOfCoins[n] !== Infinity ? numOfCoins[n] : -1;
 }
+
+
+
+var minNumberOfCoinsForChange = function (coins, amount) {
+  if (amount === 0)
+    return 0;
+
+  const map = new Map();
+  const res = findAmountRecursive(coins, amount, 0, map);
+  if (res > 99999) {
+    return -1;
+  } else {
+    return res;
+  }
+
+};
+
+const findAmountRecursive = (coins, amount, index, mem) => {
+  if (amount < 0)
+    return Number.MAX_SAFE_INTEGER;
+
+  if (amount === 0)
+    return 0;
+
+  if (!mem.has(amount)) {
+    const minCountLevel = coins.reduce((prevMin, coin, index) => {
+      if (coin <= amount) {
+        return Math.min(
+          prevMin,
+          findAmountRecursive(coins, amount - coin, index, mem) + 1
+        );
+      }
+
+      return prevMin;
+    }, Number.MAX_SAFE_INTEGER);
+
+    mem.set(amount, minCountLevel);
+  }
+
+  return mem.get(amount);
+};
