53399763f0ec182414904ec33c162ca9bd7a0662
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-08 15:40:48 -0700

    update numberOfWays

--- Algos/4. DynamicProgramming/Medium/2_NumberOfWaysToMakeChange.js
+++ Algos/4. DynamicProgramming/Medium/2_NumberOfWaysToMakeChange.js
@@ -7,19 +7,31 @@
 
 
 // todo: Do dynamic programming -- keep track of out values from 0 -> n to track number of ways to keep the numbers O(nd) time and O(n) space
+
+// n = 6, denoms [1,2] -- iterate through denoms, and at each value, find how many ways to create
+
+// 0, 1, 2, 3, 4, 5, 6
+//[1, 1, 2, 2, 3, 3, 4] -- solution in this case would be 4
+
+// pattern: if (denoms[i] <= array[j]) array[j] += array[j - denoms[i]] 
+// return the last value inside of our array
+
+// input: n = 6, denoms = [1,2]
+// output: 4
 function numberOfWaysToMakeChange(n, denoms) {
-  // create an empty array of size n + 1: [0,1,2,3,4,5] if n was equal to 5
-  const ways = new Array(n + 1).fill(0)
-  ways[0] = 1;
-  // iterate through the created array
+  // create the array of length n + 1
+  const way = new Array(n + 1).fill(0)
+  way[0] = 1;
+  // outer for loop iterating through the denoms
   for (let denom of denoms) {
-    for (let i = 1; i < ways.length; i++) { // this is going in order of the array
+    for (let i = 0; i < way.length; i++) {
       if (denom <= i) {
-        ways[i] += ways[i - denom]
+        way[i] += way[i - denom];
       }
     }
   }
-  return ways[n]
+
+  return way[n]
 }
 
-console.log(numberOfWaysToMakeChange(6, [1, 2, 5]))
+console.log(numberOfWaysToMakeChange(6, [1, 2]))
