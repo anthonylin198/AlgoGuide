78cdc34d812ac1a3351cad3b0c686b71affdcc55
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-08 10:41:08 -0700

    getPermutations update

--- Algos/1. Recursion/Medium/GetPermutations.js
+++ Algos/1. Recursion/Medium/GetPermutations.js
@@ -1,34 +1,9 @@
-// ! Prompt: Write a function that takes in an array of unique integers and returns an array of all permutations of those integers in no particular order
-// ! If the input array is empty, the function should return an emptyarry
-
-function getPermutations(array) {
-  const permutationsList = [];
-  // create an iffe recursive function that will push each permutation into the permutation list
-  (function generate(arr = array, permutation = []) {
-    // if the length of the array is empty, but permutation length exists
-    if (!arr.length) {
-      permutationsList.push(permutation);
-    } else {
-      // create a for loop, and make recursive call inside the for loop, start with a different array each time
-      for (let i = 0; i < arr.length; i++) {
-        const newArray = arr.slice(0, i).concat(arr.slice(i + 1));
-        console.log(newArray);
-        const newPermutation = permutation.concat([arr[i]]);
-        generate(newArray, newPermutation);
-      }
-    }
-  })();
-  return permutationsList;
-}
-
-console.log(getPermutations([0, 1, 2]));
-
-
 
 // Get permutations: Takes an array of unique integers and returns an array of all permutations of those integers in no particular order -- if input array is empty, function should return an empty arra
 // array = [1, 2, 3]
 // output: [[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]]
 
+// todo: recursive function using swap - O(n*n!) time and O(n*n!) space
 function getPermutations(array) {
   // create empty array to store permutations
   const permutations = [];
