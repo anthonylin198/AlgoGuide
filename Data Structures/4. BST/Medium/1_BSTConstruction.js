570c36eee1223f51f07b181aa7ee1132eb381017
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-09 13:38:35 -0700

    update construction

--- Data Structures/4. BST/Medium/1_BSTConstruction.js
+++ Data Structures/4. BST/Medium/1_BSTConstruction.js
@@ -20,6 +20,7 @@ class BST {
       } else if (value > node.value) {
         if (node.right === null) {
           node.right = new BST(value)
+          break;
         }
         node = node.right
       } else {
@@ -30,17 +31,37 @@ class BST {
     return this;
   }
 
+  // ologn -- can restructure, do if, else if, then else we know it is equal
   contains(value) {
-    // Write your code here.
+    // find if contains value
+    let node = this;
+    // while node exists
+    while (node) {
+      // check if is node.value === value , then return true
+      if (node.value === value) {
+        return true;
+      } else {
+        // else, we check if is less than or greater than
+        if (value < node.value) {
+          node = node.left
+        } else {
+          node = node.right
+        }
+      }
+    }
+    return false;
   }
 
+  // need to keep track of the parent node
   remove(value) {
     // Write your code here.
     // Do not edit the return statement of this method.
     return this;
-  }
-}
 
+  }
+
+
+}
 
 
 const bst = new BST(5)
@@ -48,7 +69,9 @@ bst.insert(2)
 bst.insert(7)
 bst.insert(9)
 bst.insert(6.5)
-bst.insert(7)
+
+
+console.log(bst.contains(6.5))
 
 
 console.log(bst)
