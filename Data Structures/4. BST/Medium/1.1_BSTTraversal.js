779b024d186385579e947b9633528657b28e4f0c
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-09 15:47:16 -0700

    update

--- Data Structures/4. BST/Medium/1.1_BSTTraversal.js
+++ Data Structures/4. BST/Medium/1.1_BSTTraversal.js
@@ -1,8 +1,19 @@
 // Write three functions that take in a bst an traverse the BST
 
 
+class Node {
+  constructor(value) {
+
+    this.value = value;
+    this.left = null;
+    this.right = null
+  }
+}
+
+
+
 // inorder traversal is from furthese left to right
-function inOrderTraverse(tree, array) {
+function inOrderTraverse(tree, array = []) {
   if (tree) {
     inOrderTraverse(tree.left, array); // this goes all the way to the end
     array.push(tree.value);
@@ -12,20 +23,31 @@ function inOrderTraverse(tree, array) {
 }
 
 // middle then left then right (not all the way down)
-function preOrderTraverse(tree, array) {
+function preOrderTraverse(tree, array = []) {
   if (tree) {
     array.push(tree.value);
-    preOrderTraverse(tree.left);
-    preOrderTraverse(tree.right)
+    preOrderTraverse(tree.left, array);
+    preOrderTraverse(tree.right, array)
   }
   return array;
 }
 
 // left, right, root
-function postOrderTraverse(tree, array) {
+function postOrderTraverse(tree, array = []) {
   if (tree) {
-    postOrderTraverse(tree.left)
-    postOrderTraverse(tree.right)
+    postOrderTraverse(tree.left, array)
+    postOrderTraverse(tree.right, array)
     array.push(tree.value);
   }
+  return array;
 }
+
+
+const bst = new Node(10)
+bst.left = new Node(5)
+bst.right = new Node(15)
+
+
+console.log(inOrderTraverse(bst, []))
+console.log(preOrderTraverse(bst, []))
+console.log(postOrderTraverse(bst, []))
