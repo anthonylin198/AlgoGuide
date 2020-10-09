b5f87d85666077d4912b73795a4fb85c2fa1a84d
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-09 15:52:38 -0700

    update

---
+++ Data Structures/4. BST/Medium/4_MinHeightBST.js
@@ -0,0 +1,8 @@
+// takes non-empty sortted array of distinct integers, constructs a BST from the integers, and returns the root of the BST
+// function shuold minimize the height of thte BST
+
+// to get the minimum height, get the midpoint number
+
+
+// once we sort, then we want to just kee adding hte midpoint number
+
