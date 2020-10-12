79417e7c826a84863aea65113f7c86c7faa304ff
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-12 10:40:24 -0700

    closures update

--- Fundamentals/closures.js
+++ Fundamentals/closures.js
@@ -1,37 +1,3 @@
-// ! 1. Object data privacy. Lexical scope means that a variable defined oustside a function can be accessible inside another function defined after the variable declaration
-const getSecret = (secret) => {
-  return {
-    get: () => secret
-  };
-};
-
-
-
-// We need to access the closure
-const tester = getSecret("yoooo").get;
-
-// console.log(tester())
-
-
-
-
-// ! 2. Partial functions: Applying the inner function to only SOME of the arguments
-const partialApply = (fn, ...fixedArgs) => {
-  return function (...remainingArgs) {
-    return fn.apply(this, fixedArgs.concat(remainingArgs));
-  };
-};
-
-
-const add = (a, b) => a + b;
-
-const add10 = partialApply(add, 10);
-
-// console.log(add10(5))
-// console.log(add10(5))
-// console.log(add10(5))
-
-
 // ! 3. Playin around - we get access to that total. We can allow others to access values inside function, without directly modifying the variables
 const adder = () => {
   let total = 0;
@@ -41,15 +7,24 @@ const adder = () => {
   }
 }
 
-const check = adder();
+
+// Use closures for data privacy simil
+const check = adder(); // set check to eqaul to the function definition of adder
+check();
 // check()
 // check()
-// check()
+
+const check2 = adder();
+check2();
+check2();
+
+
+
+
 
 
 
 // ! 4. OOP vs functional, not directly modifying the variables
-
 // OOP
 class MyClass {
 
