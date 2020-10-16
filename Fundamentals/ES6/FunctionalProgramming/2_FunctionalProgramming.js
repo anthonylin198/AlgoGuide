bebd57c539b2ee583713fc36b26a87d1389304e9
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-16 11:07:06 -0700

    functional update

---
+++ Fundamentals/ES6/FunctionalProgramming/2_FunctionalProgramming.js
@@ -0,0 +1,6 @@
+// currying allows you to take arguments one at a time. Use currying when there is an obvious configuration ot pass in and remember form then on
+// breaking down code to the pure functions
+
+// strive to write most general functions as possible - bundle down every step to the functinality
+
+// currying function, data ends up
