008f07fa7597376d936ddd407b8507f0e24bc3cc
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-19 15:03:03 -0700

    update

--- Grokking/10_PermutationandCombinations/1_permutation.js
+++ Grokking/10_PermutationandCombinations/1_permutation.js
@@ -1,6 +1,51 @@
 // input: [1, 3, 5]
 // output: [1,3,5], [1,5,3], [3,1,5], [3,5,1],[5,1,3],[5,3,1]
 
+/*
+Grokking: BFS Approach
+
+We need to keep track of the permutations in a queue
+
+
+[1]
+[3,1][1,3] insert the next value everywhere in the previous 2
+[5,3,1][3,5,1]...
+
+permutations[[3,1], [1,3] ]
+
+[1]
+
+[3,1] [1,3]
+
+
+[5]
+
+
+*/
+
+// todo: Grokking solution - [1, 2, 3] - O(O(n!))
+function getPermutations2(array) {
+  const permutations = [[array[0]]];
+  // create for loop from i = 0 to array.length
+  for (let i = 1; i < array.length; i++) {
+    // keep track of current length of permutations n = permutations.length
+    console.log(permutations);
+    const n = permutations.length;
+    for (let j = 0; j < n; j++) {
+      const perm = permutations.shift(); // get the value from before
+      const newPermLength = perm.length;
+      for (let k = 0; k < newPermLength + 1; k++) {
+        const newPerm = perm.slice();
+        newPerm.splice(k, 0, array[i]);
+        permutations.push(newPerm);
+      }
+    }
+  }
+  return permutations;
+}
+
+console.log(getPermutations2([1, 2, 3, 4]));
+
 // ! solution from algoexpertL O(n * n!) time and O(n*n!) space
 function getPermutations(array) {
   const permutations = [];
@@ -14,13 +59,12 @@ function permutationsHelper(array, currentPermutation, permutations) {
   } else {
     for (let i = 0; i < array.length; i++) {
       const newArray = array.slice(0, i).concat(array.slice(i + 1));
-      const newPermutation = currentPermutation.concat([array[i]])
-      permutationsHelper(newArray, newPermutation, permutations)
+      const newPermutation = currentPermutation.concat([array[i]]);
+      permutationsHelper(newArray, newPermutation, permutations);
     }
   }
 }
 
 // console.log(getPermutations([1, 3, 5]))
-
 
 // ! Better solution algoexpert: O(n*n!) time and O(n*n!) space
--- scratchpad.js
+++ scratchpad.js
@@ -1,47 +1,5 @@
-/*
-Product Sum: Write a function that takes in a special array and returns its product sum
+const arr = [1, 2, 3, 4, 5];
 
+arr.splice(5, 0, "insert"); //
 
-
-A special array is non-empty array that containers integers or other special arrays
-
-
-
-
-[5, 2, [7, -1], 3, [6, [-13, 8], 4]]
-
-
-
-
-
-15
-6
-22
-
-
-*/
-
-// really understand and trace through the problem NO MATTER WHAT
-
-function productSum(arr) {
-  let sum = 0;
-
-  // helper function that will add to sum
-  function helper(arr, level = 1) {
-    for (let i = 0; i < arr.length; i++) {
-      if (Array.isArray(arr[i])) {
-        helper(arr[i], (level + 1) * level);
-      } else {
-        sum += arr[i] * level;
-      }
-      console.log(sum);
-    }
-  }
-
-  helper(arr);
-
-  // after loop return sum * level
-  return sum;
-}
-
-console.log(productSum([5, 2, [7, -1], 3, [6, [-13, 8], 4]]));
+console.log(arr);
