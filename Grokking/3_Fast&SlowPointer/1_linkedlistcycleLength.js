0d3a68dcd8dc2304d878338fc5052e7a443b9fc3
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-10-19 11:44:33 -0700

    happynumber fix

--- Grokking/3_Fast&SlowPointer/1_linkedlistcycleLength.js
+++ Grokking/3_Fast&SlowPointer/1_linkedlistcycleLength.js
@@ -9,6 +9,60 @@ class Node {
   }
 }
 
+/*
+
+Find the length of the cycle with the fast and slow pointer.
+
+1 --> 2 --> 3 --> 4 --> 5 --> 6 (Cycles back to the 3)
+
+
+KEEP TRACK OF THE NODE ONCE THE INTERSECTION IS HIT
+slow: 1, 2, 3, 4 --> 5 --> 6 --> 3 --> 4
+
+fast: 2, 4, 6, 4
+
+*/
+
+function myFindCycleLength(head) {
+  let slow = head;
+  let fast = head;
+  // while loop to iterate throuhg linkedlist and find the intersection
+  while (fast && fast.next) {
+    // slow goes next
+    slow = slow.next;
+    // fast = fast.next.next
+    fast = fast.next.next;
+    // if they are equal
+    if (slow === fast) {
+      return helpFindLength(fast);
+    }
+  }
+  // else no cycle
+  return false;
+}
+
+function helpFindLength(node) {
+  let startCycle = node.next;
+  // keep track of a counter
+  let counter = 1;
+  // while node is not equal to startCycle, increment counter by 1
+  while (node !== startCycle) {
+    counter++;
+    startCycle = startCycle.next;
+  }
+  return counter;
+}
+
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
+head.next.next.next.next.next.next = head.next.next;
+console.log(`LinkedList cycle length: ${myFindCycleLength(head)}`);
+head.next.next.next.next.next.next = head.next.next.next;
+console.log(`LinkedList cycle length: ${myFindCycleLength(head)}`);
 
 function find_cycle_length(head) {
   let slow = head,
@@ -16,7 +70,8 @@ function find_cycle_length(head) {
   while (fast !== null && fast.next !== null) {
     fast = fast.next.next;
     slow = slow.next;
-    if (slow === fast) { // found the cycle
+    if (slow === fast) {
+      // found the cycle
       return calculate_cycle_length(slow);
     }
   }
@@ -27,18 +82,11 @@ function calculate_cycle_length(slow) {
   let current = slow,
     cycle_length = 0;
   while (true) {
-    current = current.next; cycle_length += 1;
+    current = current.next;
+    cycle_length += 1;
     if (current === slow) {
       break;
     }
   }
   return cycle_length;
 }
-
-
-const head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-head.next.next.next = new Node(4);
-head.next.next.next.next = new Node(5); head.next.next.next.next.next = new Node(6); head.next.next.next.next.next.next = head.next.next; console.log(`LinkedList cycle length: ${find_cycle_length(head)}`);
-head.next.next.next.next.next.next = head.next.next.next; console.log(`LinkedList cycle length: ${find_cycle_length(head)}`);
--- Grokking/3_Fast&SlowPointer/2_HappyNumber.js
+++ Grokking/3_Fast&SlowPointer/2_HappyNumber.js
@@ -1,6 +1,91 @@
-// a happy number will eventually lead us to the number 1. Determine if a number is happy or not
+// a happy number will eventually lead us to the number 1. Determine if a number is happy or not.
 // input: 23
-// output: true 
+// output: true
+
+/*
+
+input: 23 
+4 + 9 = 13
+
+1 + 9 = 10
+
+1 + 0 = 1
+
+Output: true
+
+
+input: 82
+64 + 4 = 68
+34 + 64 = 100
+1 + 0 + 0 = 1
+
+input: 83
+64 + 9 = 73
+49 + 4 = 53
+25 + 9 = 34
+9 + 16 = 25
+4 + 25 = 29
+4 + 81 = 85
+64 + 25 = 89 ----> CYCLE
+64 + 81 = 145
+1 + 16 + 25 = 42
+16 + 4 = 20
+4 = 4
+16 = 16
+1 + 36 = 37
+9 + 49 = 58
+25 + 64 = 89 ---> CYCLE
+
+
+output: false --> goes in a never ending cycle
+
+
+Solution: fast and slow pointer approach -- we know that if the fast pointer enters the cycle, the slow and fast pointer will inevitably meet
+
+each iteration, check if square sum is 1, if it is then return true
+
+else check if the slow pointer is equal to fast pointer, if they are equal return false
+
+
+through the while loop, we will have to perform square digit O(n) time
+
+
+*/
+
+function myHappyNumber(num) {
+  // create slow variable
+  let slow = num;
+  // create fast variable
+  let fast = num;
+  // while true
+  while (true) {
+    // O(logn)
+    slow = squareDigits(slow);
+    fast = squareDigits(squareDigits(fast)); //O(3d)
+    if (fast === 1) {
+      return true;
+    }
+    if (fast === slow) {
+      return false;
+    }
+  }
+}
+
+function squareDigits(num) {
+  // change num to a string
+  const digitStr = num.toString();
+  // keep track of the sum
+  let sum = 0;
+  for (let i = 0; i < digitStr.length; i++) {
+    sum += Math.pow(parseInt(digitStr[i]), 2);
+  }
+  // return the value
+  console.log(sum);
+  return sum;
+}
+
+console.log(myHappyNumber(23));
+console.log(myHappyNumber(83));
 
 // Solution: Similar  to the linkedlist cycle
 // Time Complexity: O(logN) because if the number N is less than or equal to 2000, we reach the cycle or 1 in at most 1001 steps. N1<81M 9 squared is the highest possible)
@@ -10,8 +95,9 @@ function find_happy_number(num) {
     fast = num;
   while (true) {
     slow = find_square_sum(slow); // move one step
-    fast = find_square_sum(find_square_sum(fast)); // move two steps 
-    if (slow === fast) { // found the cycle
+    fast = find_square_sum(find_square_sum(fast)); // move two steps
+    if (slow === fast) {
+      // found the cycle
       break;
     }
   }
@@ -19,7 +105,7 @@ function find_happy_number(num) {
 }
 function find_square_sum(num) {
   let sum = 0;
-  while ((num > 0)) {
+  while (num > 0) {
     digit = num % 10;
     sum += digit * digit;
     num = Math.floor(num / 10);
