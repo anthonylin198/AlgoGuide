ccb618190da70f97e25e6b5b93553d689b286580
Anthony Lin <anthony.lin.198@gmail.com>
2020-10-26 12:32:47 -0700

    update

--- Leetcode/Easy/7.1_ReverseLinkedLists.js
+++ Leetcode/Easy/7.1_ReverseLinkedLists.js
@@ -9,9 +9,9 @@
 Doing this in place: 
 
 
-keep track of the previous node, as well as the current node
+keep track of the previous node, as well as the current node. Previous null should start at null
 
-1 -> null                      2 -> 3 -> 4 -> 5
+1 -> null                      2 -> 3 -> 4 -> 5     make sure to keep track of the nextnode
 2 -> 1 -> null                 3 -> 4 -> 5
 3 -> 2 -> 1 -> null            4 -> 5
 4 -> 3 -> 2 -> 1 -> null       5
@@ -29,7 +29,7 @@ function reverse(head) {
   // create while loop to iterate through the linked list
   while (currNode) {
     // must keep track of the next node
-    const nextNode = currNode.next; // we initialize it up here, so it does not change
+    const nextNode = currNode.next; // we initialize it up here, so it does not change as the linkedlist is being manipulated
     // set currnode.next to the prev node
     currNode.next = prevNode;
     // set prevnode to equal current node
