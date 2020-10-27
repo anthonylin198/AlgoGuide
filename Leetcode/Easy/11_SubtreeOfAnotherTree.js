0393778de1214c0c0226f019d12695e255789a54
Anthony Lin <anthony.lin.198@gmail.com>
2020-10-27 12:15:53 -0700

    subtree of another update

--- Leetcode/Easy/11_SubtreeOfAnotherTree.js
+++ Leetcode/Easy/11_SubtreeOfAnotherTree.js
@@ -65,7 +65,9 @@ var isSubtree = function (s, t) {
   var isSame = function (node1, node2) {
     if (!node1 && !node2) return true;
     if (!node1 || !node2 || node1.val != node2.val) return false;
-    return isSame(node1.left, node2.left) && isSame(node1.right, node2.right);
+    let left = isSame(node1.left, node2.left);
+    let right = isSame(node1.right, node2.right);
+    return left && right;
   };
 
   // whether t is a subtree of the given node
