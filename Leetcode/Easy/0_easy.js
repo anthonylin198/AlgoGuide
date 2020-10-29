8c974e1eaeae50f8f6fb0da4f21358b918b3aa51
Anthony Lin <anthony.lin.198@gmail.com>
2020-10-29 11:09:19 -0700

    changes update

--- Leetcode/Easy/0_easy.js
+++ Leetcode/Easy/0_easy.js
@@ -18,15 +18,26 @@ REGEX METHODS
 
 
 
-slice and substr essentially do the same. Can just add together
+SLICE AND SUBSTRING essentially do the same. Can just add together
 
-arr.slice(0, i) + arr.slice(i+1) this would get rid of the middle character each time
+  arr.slice(0, i) + arr.slice(i+1) this would get rid of the middle character each time
 
 */
 
 // todo: Sorting
+/*
+SORT METHOD: return -1 for in order an 1 for not in order
 
-// todo: Binary Search - Understand when to use the while, and when not to.
+
+MANUAL: Be able to use 2 pointers and manually sort as you need. Learn some of 
+the other sorting algorithms later as well
+
+/*
+
+
+// todo: Binary Search - Divide and Conquer
+
+// todo: Carry Over : Adding strings or binary
 
 // todo: Creating classes - Taking the average sum
 
