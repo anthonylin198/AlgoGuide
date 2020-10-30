d10777589ea2332163fce8da7f7a16f4d61852b6
Anthony Lin <anthony.lin.198@gmail.com>
2020-10-30 11:25:01 -0700

    merge two lists

--- Leetcode/Easy/7_MergeTwoSortedLists.js
+++ Leetcode/Easy/7_MergeTwoSortedLists.js
@@ -54,75 +54,58 @@ class Node {
   }
 }
 
-function mergeLinkedLists(l1, l2) {
+var mergeTwoLists = function (l1, l2) {
+  var mergedHead = { val: -1, next: null },
+    crt = mergedHead;
+  while (l1 && l2) {
+    if (l1.val > l2.val) {
+      crt.next = l2;
+      l2 = l2.next;
+    } else {
+      crt.next = l1;
+      l1 = l1.next;
+    }
+    crt = crt.next;
+  }
+  crt.next = l1 || l2;
+
+  return mergedHead.next;
+};
+
+// todo: Iterative AlgoExpert in Place
+function mergeLinkedLists2(l1, l2) {
   p1 = l1;
   p1Prev = null;
   p2 = l2;
   // create loop to iterate
   while (p1 && p2) {
-    if (p1.value < p2.value) {
+    if (p1.val < p2.val) {
       // if the value in p1 is less than value in p2, then continue
       p1Prev = p1;
       p1 = p1.next;
     } else {
       // insert before
-      if (p1Prev !== null) p1Prev.next = p2; //
-      // else if is the first value
-      p1Prev = p2;
-      p2 = p2.next; // we make p2 the next
-      p1Prev.next = p1;
-    }
-  }
-  if (p1 === null) p1Prev.next = p2; // if we hit hte end of p1, the p1prev.next is p2
-  return l1.value < l2.value ? l1 : l2; // return one of them
-}
-
-// merging to p1 as the original
-function mergeTwoLists(l1, l2) {
-  p1 = l1;
-  p1Prev = null;
-  p2 = l2;
-  while (p1 && p2) {
-    if (p1.val < p2.val) {
-      p1Prev = p1;
-      p1 = p1.next;
-    } else {
-      // if less than or equal to, then we must make value at p2 point to p1
-      if (p1Prev !== null) p1Prev.next = p2;
-      // if not equal to null we must do this
-      p1Prev = p2; // because now p1Prev is changed, so this DOES NOT overwrite it
-      p2 = p2.next; // remember we need to iterate through
-      p1Prev.next = p1;
-    }
-  }
-  if (p1 === null) p1Prev.next = p2; // if we reached the end of p1 first, must tack on the rest of p2
-  // because of the initial manipulation, we must change
-  return l1.value < l2.value ? l1 : l2; // remember that we are returning hte entire l1 and l2
-}
-
-function mergeTwoList2(l1, l2) {
-  // keep track of 3 pointers, p1, p2, and p1Prev
-  let p1 = l1;
-  let p2 = l2;
-  let p1Prev = null;
-  // create while loop iterating while p1 and p2 both exists
-  while (p1 && p2) {
-    // if value at p1 is less than value at p2, just continue, set p1 and p1Prev
-    if (p1.val < p2.val) {
-      p1Prev = p1;
-      p1 = p1.next;
-    } else {
-      if (p1Prev) p1Prev.next = p2;
-      p1Prev = p2;
+      if (p1Prev !== null) p1Prev.next = p2; // if p1Prev is not null
+      p1Prev = p2; // when setting this, does not change l1 only changes the p1Prev
       p2 = p2.next;
       p1Prev.next = p1;
     }
   }
-
-  // outside while if we reach end of p1 before p2, p1Prev.next = p1. If we do null no good
-
-  // return the correct list
+  if (p1 === null) p1Prev.next = p2;
+  return l1.value < l2.value ? l1 : l2; // return one of them
 }
+
+// todo: Recursive Solution
+var mergeTwoLists3 = function (l1, l2) {
+  if (!l1 || !l2) return l1 ? l1 : l2; // exit condition
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists3(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists3(l1, l2.next);
+    return l2;
+  }
+};
 
 const l1 = new Node(1);
 l1.next = new Node(2);
--- scratchpad.js
+++ scratchpad.js
@@ -1,46 +1,74 @@
 /*
 
+Merge Two Sorted Lists:
 
-Write a function that takes the head of a singly linked list and integer k, and removes the kth node from the end
+1 - 2 -> 4
+1 - 3 -> 4
 
-0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
-k = 4
-
-0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 7 -> 8 -> 9
-
-
-What should we do if k is greater than node. At least k nodes
-
+1 -> 1 -> 2 -> 3 -> 4 -> 4
 
 
 */
 
 /*
 
-0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
-k = 4
+Create test case
 
-Must remove the node at: length of linked list - k + 1
-
-
-2 pointers - one pointer start from 0 and iterate until the end, second pointer starts from 0 and interates once first pointer hits k
-
-if p1.next is null, then we need to remove the 6th node
-counter = 4
-p1= null
-p2= 5   -- point to the 7
-
-0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
-                         p         p
+l1: 1 -> 3 -> 7
+l2: 0 -> 2 -> 5 -> 8 -> 10
 
 
 
-Solution: O(n) time and O(1) space
+merge nodes into the linkedlist
+
+p1: 3
+p2: 2
+prevNode: 1 -> 2 -> 3 -> 7
+
+l1: 0 -> 1 -> 3 -> 7
 
 
+inserting the node in list 2 each time it is less than l1 
+
+
+edge case: if the prev node is null, then we insert before at the head
 
 */
 
+function mergeTwoLists(l1, l2) {
+  let p1 = l1;
+  let p2 = l2;
+  let prevNode = null;
+  // while p2 and p1 exists
+  while (p1 && p2) {
+    if (p2.val <= p1.val) {
+      // if prevNode is null
+      if (prevNode === null) {
+        // inserting at the head
+        const temp = p2.next;
+        p2.next = p1; // 1 -> 1 -> 2 -> 4
+        p2 = temp;
+      } else {
+        const temp = prevNode.next; // 3 -> 7
+        prevNode.next = p2; // 1 -> 2 -> 5 -> 8 -> 10
+        prevNode.next.next = temp; // 1 -> 2 -> 3 -> 7
+        p2 = p2.next;
+      }
+    } else {
+      p1 = p1.next;
+      prevNode = p1;
+    }
+  }
+  // if p2 still exists
+  if (p2) {
+    p1 = p2;
+  }
+  return l2;
+}
+
+// insert before helper function
+
+// This is an input class. Do not edit.
 class LinkedList {
   constructor(value) {
     this.value = value;
@@ -50,41 +78,79 @@ class LinkedList {
 
 /*
 
-p1=9
-p2=5
-counter=4
-0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
+Create test case
 
-0 -> 1 -> 2 -> 3 -> 4
+l1: 1 -> 3 -> 7
+l2: 0 -> 2 -> 5 -> 8 -> 10
 
 
+
+merge nodes into the linkedlist
+
+p1: 3 -> 7
+p2: 5 - 8 -> 10
+p1Prev: 2 -> 3 -> 7
+
+l1: 0 -> 1 -> 2 -> 5 -> 8 -> 10
+l2: 0 -> 2 -> 5 -> 8 -> 10
+
+inserting the node in list 2 each time it is less than l1 
+
+
+edge case: if the prev node is null, then we insert before at the head
 
 */
 
-// we need to be able to handle the edge case of removing the first node. What if k is equal to the full length
-function removeKthNodeFromEnd(head, k) {
-  // create p1 and p2
-  let p1 = head;
-  let p2 = head;
-  let counter = 0;
-
-  // while p1.next is not null
-  while (p1.next) {
-    if (counter === k) {
-      p1 = p1.next;
-      p2 = p2.next;
+// todo: Merge two lists solution
+var mergeTwoLists = function (l1, l2) {
+  var mergedHead = { val: -1, next: null },
+    crt = mergedHead;
+  while (l1 && l2) {
+    if (l1.val > l2.val) {
+      crt.next = l2;
+      l2 = l2.next;
     } else {
+      crt.next = l1;
+      l1 = l1.next;
+    }
+    crt = crt.next;
+  }
+  crt.next = l1 || l2;
+
+  return mergedHead.next;
+};
+
+// todo: Iterative AlgoExpert in Place
+function mergeLinkedLists2(l1, l2) {
+  p1 = l1;
+  p1Prev = null;
+  p2 = l2;
+  // create loop to iterate
+  while (p1 && p2) {
+    if (p1.val < p2.val) {
+      // if the value in p1 is less than value in p2, then continue
+      p1Prev = p1;
       p1 = p1.next;
-      counter++;
+    } else {
+      // insert before
+      if (p1Prev !== null) p1Prev.next = p2; // if p1Prev is not null
+      p1Prev = p2; // when setting this, does not change l1 only changes the p1Prev
+      p2 = p2.next;
+      p1Prev.next = p1;
     }
   }
-  if (k - counter >= 1) {
-    // means k is too large
-    head.value = head.next.value;
-    head.next = head.next.next;
-    return;
-  }
-  const temp = p2.next.next;
-  p2.next = temp;
-  return;
+  if (p1 === null) p1Prev.next = p2;
+  return l1.value < l2.value ? l1 : l2; // return one of them
 }
+
+// todo: Recursive Solution
+var mergeTwoLists3 = function (l1, l2) {
+  if (!l1 || !l2) return l1 ? l1 : l2; // exit condition
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists3(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists3(l1, l2.next);
+    return l2;
+  }
+};
