caec863620d7e292fb18f5ba58b41f0ea92c4f61
Anthony Lin <anthony.lin.198@gmail.com>
2020-10-31 13:04:47 -0700

    update questions

--- scratchpad.js
+++ scratchpad.js
@@ -61,35 +61,3 @@ function rangeSum(root, L, R) {
   sumHelper(root, L, R);
   return sum;
 }
-
-// todo: lowest common ancestor for bst
-var lowestCommonAncestor = function (root, p, q) {
-  while (root) {
-    if (root.val < p.val && root.val < q.val) {
-      root = root.right;
-    } else if (root.val > p.val && root.val > q.val) {
-      root = root.left;
-    } else {
-      break;
-    }
-  }
-  return root;
-};
-
-function lowestCommonAncestor(root, p, q) {
-  if (root.val < p.val && root.val < q.val) {
-    return lowestCommonAncestor(root.right, p, q);
-  }
-  if (root.val > p.val && root.val > q.val) {
-    return lowestCommonAncestor(root.left, p, q);
-  }
-  return root;
-}
-
-// todo: lowest common ancestor for binary tree
-function lowestCommonAncestor(root, p, q) {
-  if (!root || root === p || root === q) return root;
-  var resL = lowestCommonAncestor(root.left, p, q);
-  var resR = lowestCommonAncestor(root.right, p, q);
-  return resL && resR ? root : resL || resR;
-}
