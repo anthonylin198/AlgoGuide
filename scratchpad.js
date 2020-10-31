f55ba20420a71c3511a4f0b33882d3176f7c008d
Anthony Lin <anthony.lin.198@gmail.com>
2020-10-31 13:03:45 -0700

    update pad

--- scratchpad.js
+++ scratchpad.js
@@ -1,57 +1,95 @@
 /*
 
-Subtree of another subtree. Every time we see a value that is equal to hte node.value, we check if that subtree is exactly identical to the given tree
+Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
 
-BETTER UNDERSTAND THE CALL STACK, WHY DOESN'T YOUR SOLUTION WORK. Better understanding of traversal, then you can logic your way through this question
+The binary search tree is guaranteed to have unique values.
+
+Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+Output: 32
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+Output: 23
+
+
 
 */
 
 /*
 
-todo: Solution -- iterate through the binary tree, if we see a value that is equal to the tree we want to match, check equality. if it is equal set to true
+                                          20
+                              10                    30
+                           5       15                     40
+                        1            20
+  Left: 1
+  Right: 30
+
+  1 + 5 + 10 + 15 + 20 + 30
 
 
-                      3
-                    4    4
-                  1  2
-                    
+  Left: 5
+  Right: 30
+
+  5 + 10 + 15  
 
 
+  1) find the most common parent of the 2 nodes
+    -- search of the left node -- root.left.left
+    -- search for the right node root.right 
+         -- most common is the root
+  
+  2) Find the sum from the most common parent
 
 
-
+  
+                      
 */
 
-// todo: Find a way to save the value of the checked. if check is every true se cna reutrn
-function isSubtree(s, t) {
-  // create a variable to check
-  let check = false;
-  // create helper function for dfs through the tree
-  function dfs(s, t) {
-    if (!s || check === true) {
+function rangeSum(root, L, R) {
+  // keep track of the sum
+  let sum = 0;
+  // create helper function for dfs, only add if between
+  function sumHelper(root, L, R) {
+    // if !root
+    if (!root) {
       return;
+    } else if (root.val >= L && root.val <= R) {
+      sum += root.val;
     }
-    if (s.val === t.val) {
-      check = checkEqual(s, t);
-    }
-    dfs(s.left, t);
-    dfs(s.right, t);
+    sumHelper(root.left, L, R);
+    sumHelper(root.right, L, R);
   }
-  dfs(s, t);
-  // return the check variable
-  return check;
+  sumHelper(root, L, R);
+  return sum;
 }
 
-// create helper function to check equality between the trees
-function checkEqual(s, t) {
-  if (!s && !t) {
-    return true;
+// todo: lowest common ancestor for bst
+var lowestCommonAncestor = function (root, p, q) {
+  while (root) {
+    if (root.val < p.val && root.val < q.val) {
+      root = root.right;
+    } else if (root.val > p.val && root.val > q.val) {
+      root = root.left;
+    } else {
+      break;
+    }
   }
+  return root;
+};
 
-  if ((!s && t) || (s && !t) || s.val !== t.val) {
-    return false;
+function lowestCommonAncestor(root, p, q) {
+  if (root.val < p.val && root.val < q.val) {
+    return lowestCommonAncestor(root.right, p, q);
   }
-  // recursive call to the left for s and t
-  checkEquals(s.left, t.left);
-  checkEquals(s.right, t.right);
+  if (root.val > p.val && root.val > q.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  }
+  return root;
+}
+
+// todo: lowest common ancestor for binary tree
+function lowestCommonAncestor(root, p, q) {
+  if (!root || root === p || root === q) return root;
+  var resL = lowestCommonAncestor(root.left, p, q);
+  var resR = lowestCommonAncestor(root.right, p, q);
+  return resL && resR ? root : resL || resR;
 }
