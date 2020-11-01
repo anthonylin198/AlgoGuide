643512906b48867ff512573ad9e0811dca63f04e
Anthony Lin <anthony.lin.198@gmail.com>
2020-11-01 14:43:03 -0800

    subset update

--- Leetcode/Medium/2_Subsets.js
+++ Leetcode/Medium/2_Subsets.js
@@ -44,3 +44,18 @@ function subsets(nums) {
   }
   return subsets;
 }
+
+// todo: Recursive solution
+function subsets(nums) {
+  const powerset = [];
+  generatePowerset([], 0);
+
+  function generatePowerset(path, index) {
+    powerset.push(path);
+    for (let i = index; i < nums.length; i++) {
+      generatePowerset([...path, nums[i]], i + 1);
+    }
+  }
+
+  return powerset;
+}
