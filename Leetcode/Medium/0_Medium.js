fa07a82eb1081d7ec7ebec7c96172282c1b3ec03
Anthony Lin <anthony.lin.198@gmail.com>
2020-11-06 09:17:43 -0800

    medium fixup

--- Leetcode/Medium/0_Medium.js
+++ Leetcode/Medium/0_Medium.js
@@ -1,4 +1,17 @@
-// Graphs (store v vertices/nodes, and e edges) O(V+E) space and O(V+E) time
+// todo Permutations and Subsets
+/*
+1) Use the depth first search approach, always taking the elements before and adding
+2) With All combinations start with beginning and make slices
+3) With the permutations we need to insert at every index, we need to shift. Triple nested for loop - the regular arr, then the array itself, then adding at every index
+
+
+
+
+
+
+*/
+
+// todo Graphs (store v vertices/nodes, and e edges) O(V+E) space and O(V+E) time
 /*
 
 1) Connected, cyclical/acyclical, and directional are the key concepts
--- scratchpad.js
+++ scratchpad.js
@@ -1,110 +1,61 @@
 /*
 
-You're given a list of arbitrary jobs that need to be completed; these jobs are represented by distinct integers. Also given a list of dependencies.
+Given a collection of distinct integers, return all possible permutations.
 
-A dependency is represented as a pair of jobs where the first job is a prerequisite of the second one. In other words, the second job depends on the first one; it can only be completed
-once the first job is completed
 
-Write a function that takes in a list of jobs and list of dependencies and returns a list containing valid order which given jobs can be completed
-
-jobs = [1,2,3,4]
-deps = [[1,2,], [1,3], [3,2], [4,2], [4,3]]
-
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
 
 
 */
 
 /*
 
-If we use a graph, we will be able to solve this question in O(j + d) time and O(j + d) space
-
-         1     prereqs  []      visited= false    visiting = false
-         2     prereqs: [1]
-         3     prereqs: [1]
-         4     prereqs: []
+Return all possible permutations with the input of [1,2,3]      1,2,3    1,3,2     2,1,3  2,3,1        3,1,2    3,2,1
 
 
-Construct a graph of all jobs, and each jobnode will have the prereqs
+[1]
+[2,1] , [1,2]
+[3,2,1] [2,3,1] [2,1,3]          [3,1,2][1,3,2][1,2,3]
 
 */
 
-class JobGraph {
-  constructor(jobs) {
-    this.nodes = [];
-    this.graph = {};
-    for (const job of jobs) {
-      this.addNode(job);
+/*
+
+current = [1]
+
+permutations = [[2,1]]
+
+newPerm = [2,1]
+
+
+*/
+
+function permutations(arr) {
+  const permutations = [[arr[0]]];
+  // for loop iterating through arr from index 1 to arr.length
+  for (let i = 1; i < arr.length; i++) {
+    // get the current lenght of the array
+    const currentLength = permutations.length;
+    for (let j = 0; j < currentLength; j++) {
+      const current = permutations.shift();
+      for (let k = 0; k < current.length + 1; k++) {
+        const newPerm = current.slice();
+        newPerm.splice(k, 0, arr[i]);
+        permutations.push(newPerm);
+      }
     }
   }
-  addPrereq(job, prereq) {
-    const jobNode = this.getNode(job);
-    const prereqNode = this.getNode(prereq);
-    jobNode.prereqs.push(prereqNode);
-  }
-  addNode(job) {
-    this.graph[job] = new JobNode(job);
-    this.nodes.push(this.graph[job]);
-  }
-  getNode(job) {
-    if (!(job in this.graph)) this.addNode(job);
-    return this.graph[job];
-  }
+  // return permutations
+  return permutations;
 }
 
-class JobNode {
-  constructor(job) {
-    this.job = job;
-    this.prereqs = [];
-    this.visited = false;
-    this.visiting = false;
-  }
-}
-
-function topologicalSort(jobs, deps) {
-  // first create the graph - add in new jobNodes, then add all the prereqs
-  const jobGraph = new JobGraph(jobs, deps);
-  for (const [prereq, job] of deps) {
-    jobGraph.addPrereq(job, prereq);
-  }
-  // return the jobs in order
-  return getOrderedJobs(jobGraph);
-}
-
-// function to get the ordered jobs
-function getOrderedJobs(graph) {
-  const orderedJobs = [];
-  const { nodes } = graph;
-  while (nodes.length) {
-    const node = nodes.pop();
-    const containsCycle = depthFirstTraverse(node, orderedJobs);
-    if (containsCycle) return [];
-  }
-  return orderedJobs;
-}
-
-function depthFirstTraverse(node, orderedJobs) {
-  if (node.visited) return false;
-  if (node.visiting) return true;
-  node.visiting = true;
-  for (const prereqNode of node.prereqs) {
-    const containsCycle = depthFirstTraverse(prereqNode, orderedJobs);
-    if (containsCycle) return true;
-  }
-  node.visited = true;
-  node.visiting = false;
-  orderedJobs.push(node.job); // if reaches this point, mean that all prereqs are hit
-  return false;
-}
-
-console.log(
-  topologicalSort(
-    [1, 2, 3, 4],
-    [
-      [1, 2],
-      [1, 3],
-      [3, 2],
-      [4, 2],
-      [4, 3],
-    ]
-  )
-);
+console.log(permutations([1, 2, 3]));
